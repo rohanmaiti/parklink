d0fd495023eaa6cf90c2d5797aac2453972b6ada
rohanmaiti <maitirohan2019@gmail.com>
2025-03-20 09:55:11 +0530

    server updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -82,9 +82,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/parking", parkingRoutes);
 
 const __dirname = path.resolve();
-if(process.env.NODE_ENV == "production"){
+// if(process.env.NODE_ENV == "production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
   app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
   })
-}
+// }
